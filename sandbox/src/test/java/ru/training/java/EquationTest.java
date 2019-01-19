b1c73ee0125221ed848bd3cdd5c894aba38f1480
Aleksandr Matjushenko <guevara3647@gmail.com>
2019-01-19 18:32:08 +0300

    Created class Equation

---
+++ sandbox/src/test/java/ru/training/java/EquationTest.java
@@ -0,0 +1,25 @@
+package ru.training.java;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTest {
+
+    @Test
+    public  void  test0(){
+        Equation eq = new Equation(1,1,1);
+        Assert.assertEquals(eq.rootNumber(),0);
+    }
+
+    @Test
+    public  void  test1(){
+        Equation eq = new Equation(1,2,1);
+        Assert.assertEquals(eq.rootNumber(),1);
+    }
+    @Test
+    public  void  test2(){
+        Equation eq = new Equation(1,5,6);
+        Assert.assertEquals(eq.rootNumber(),2);
+    }
+
+}
